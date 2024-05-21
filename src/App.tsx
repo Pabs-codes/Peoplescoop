63f383c59fbec0440e4a041f7ca33efca852841e
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-05-21 15:40:29 +0530

    first push

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import ResponsiveAppBar from './Components/Appbar'
+import SignInSide from './Components/SignInSide'
+
+function App() {
+  
+
+  return (
+    <>
+      
+
+        <ResponsiveAppBar/>
+        <SignInSide/>
+       
+    </>
+  )
+}
+
+export default App
